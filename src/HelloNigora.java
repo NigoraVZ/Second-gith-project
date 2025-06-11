a32502218f6cbdb947cc79682e709ba21ab7346b
NigoraVZ <Nigoravz@gmail.com>
2025-06-11 15:00:18 -0400

    Update HelloNigora.java

--- src/HelloNigora.java
+++ src/HelloNigora.java
@@ -1,6 +1,8 @@
 public class HelloNigora {
     public static void main(String[] args) {
         System.out.println("Hello Nigora");
+        System.out.println("Hello George");
+         System.out.println("Hello Cookie")
     }
 
 }
